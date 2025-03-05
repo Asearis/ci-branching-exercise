93c9112f17a7ef3948a8e62d6d36fc5a4a644506
Monish Sunku Prabhakar <monishsp@gmail.com>
2025-03-05 14:24:17 -0800

    Introducing good code

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/new-endpoint')
+def bad_endpoint():
+    value = "This is bad code"
+    for i in range(10):  # Unused loop variable
+        x = i * 2  # Unused variable
+    return value  # No proper JSON response
 
 if __name__ == "__main__":
     app.run(debug=True)
